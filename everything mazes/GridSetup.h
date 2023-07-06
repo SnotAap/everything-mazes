1a6c38311617f3f6d83cf8cda12f039c919cd02b
Levi <levivanwingerden@hotmail.com>
2023-07-06 15:05:24 +0200

    added prims maze generation algorithm to the gridsetup header file

--- everything mazes/GridSetup.h
+++ everything mazes/GridSetup.h
@@ -2,4 +2,6 @@
 
 void gridSetup(int sizeX, int sizeY, std::vector<std::shared_ptr<Object>>& renderList, std::map<cordinates, std::shared_ptr<Tile>>& tileMap, std::map<cordinates, walls>& wallMap, std::vector<cordinates>& expressedwallMap);
 
-std::pair<cordinates, cordinates> generateMaze(int sizeX, int sizeY, std::map<cordinates, walls>& wallMap, std::map<cordinates, std::shared_ptr<Tile>>& tileMap);
+std::pair<cordinates, cordinates> recursiveBacktrackingMaze(int sizeX, int sizeY, std::map<cordinates, walls>& wallMap, std::map<cordinates, std::shared_ptr<Tile>>& tileMap);
+
+std::pair<cordinates, cordinates> primsMaze(int sizeX, int sizeY, std::map<cordinates, walls>& wallMap, std::map<cordinates, std::shared_ptr<Tile>>& tileMap);
