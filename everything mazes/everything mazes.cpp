0c8bdb7c333e9525cd8ad6161b8ff15f6fda7f72
Levi <levivanwingerden@hotmail.com>
2023-07-15 09:15:08 +0200

    updated the main loop to work with the new infrastructure

--- everything mazes/everything mazes.cpp
+++ everything mazes/everything mazes.cpp
@@ -1,36 +1,39 @@
 #include "Header.h"
 
 std::vector<std::shared_ptr<Object>> renderList;
-std::map<cordinates, walls> wallMap;
-std::map<cordinates, std::shared_ptr<Tile>> tileMap;
 std::vector<cordinates> expressedwallMap;
 std::pair<cordinates, cordinates> startAndEndCords;
 std::vector<cordinates> path;
 int sizeX = 20;
 int sizeY = 20;
 
+#define generateState 0
+#define solveState 1
+int state = 0;
+
+unsigned long currentTime;
+unsigned long lastTime;
+unsigned int deltaTime;
 
 int main()
 {
-
-    srand(unsigned int(time(NULL)));
+    lastTime = timeGetTime();
+    srand((unsigned int)timeGetTime());
+    Grid grid(sizeX, sizeY);
+    grid.setup(renderList, expressedwallMap);
 
     sf::RenderWindow window(sf::VideoMode(1920, 1080), "Mazes!");
-    gridSetup(sizeX, sizeY, renderList, tileMap, wallMap, expressedwallMap);
-    startAndEndCords = recursiveBacktrackingMaze(sizeX, sizeY, wallMap, tileMap);
-    //startAndEndCords = primsMaze(sizeX, sizeY, wallMap, tileMap);
-    
+
+    startAndEndCords = grid.recursiveBacktrackingMaze(renderList, deltaTime);
+    //startAndEndCords = grid.primsMaze(renderList, deltaTime);
+
     for (int i = 0; i < expressedwallMap.size(); i++)
     {
         for (int j = 0; j < 4; j++)
         {
-            if (wallMap[expressedwallMap[i]][j]->active)
-            {
-                renderList.emplace_back(wallMap[expressedwallMap[i]][j]);
-            }
+                renderList.emplace_back(grid.wallMap[expressedwallMap[i]][j]);
         }   
     } 
-
     while (window.isOpen())
     {
         sf::Event event;
@@ -41,13 +44,16 @@ int main()
         }        
         window.clear(sf::Color::White);
         //
-        
+        currentTime = timeGetTime();
+        deltaTime = currentTime - lastTime;
+        //
         for (int i = 0; i < renderList.size(); i++)
         {
             renderList[i]->draw(&window);
         }
         //
         window.display();
+        lastTime = currentTime;
     }
     return 0;
 }
