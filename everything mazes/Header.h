a165b0d581c1cf4a75a2e3d2b7702fd807727661
Levi <levivanwingerden@hotmail.com>
2023-07-17 21:45:47 +0200

    update the header to support the deletion and addition of files

--- everything mazes/Header.h
+++ everything mazes/Header.h
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include "Windows.h"
+#include <string>
 #include <time.h>
 #include <vector>
 #include <unordered_map>
@@ -19,10 +20,10 @@
 #include "Line.h"
 #include "Wall.h"
 #include "CustomTypes.h"
-#include "Structs.h"
 #include "Tile.h"
 #include "Grid.h"
 #include "PathFinding.h"
 #include "Robot.h"
+#include "Robot0.h"
 
 
